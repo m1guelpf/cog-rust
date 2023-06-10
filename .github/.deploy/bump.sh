16f5a41750f6de8616e5886b17bd849dd9bb7fd6
Miguel Piedrafita <github@miguel.build>
2023-06-10 22:27:34 +0100

    Fix release process

--- .github/.deploy/bump.sh
+++ .github/.deploy/bump.sh
@@ -2,6 +2,10 @@
 
 version=$(git describe --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/')
 
+if [[ $version != *.*.* ]]; then
+  version="$version.0"
+fi
+
 echo "Bumping Cargo version to $version"
 
 # Replace the version in the Cargo.toml file with the $version variable
